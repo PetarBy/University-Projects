bcc22d219fec27940cbe191e8fe548b2493560a9
PetarBy <peterbyalkov@gmail.com>
2025-05-25 19:49:28 +0300

    Update SickLeave.java

--- Medical Record System Project/medical_record_system/src/main/java/com/medical_record_system/model/entity/SickLeave.java
+++ Medical Record System Project/medical_record_system/src/main/java/com/medical_record_system/model/entity/SickLeave.java
@@ -1,9 +1,20 @@
 package com.medical_record_system.model.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "sick_leave")
@@ -26,6 +37,10 @@ public class SickLeave {
 
     @Column(name = "end_date", nullable = false)
     private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor issuedByDoctor;
 
     @Column(columnDefinition = "TEXT")
     private String reason;
